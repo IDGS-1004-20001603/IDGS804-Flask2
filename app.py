3818f66868dd26b7df1d2cf7dcf98a5ec0622ff3
= <=>
2023-02-23 12:10:14 -0600

    Se finalizo la actividad de traductor y además se agrega el tema de cookies 22/02/2023

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, flash
+from flask import request, make_response
 import forms
 from flask_wtf.csrf import CSRFProtect
 from collections import Counter
 import os
 
 app = Flask(__name__)
-# SECRET_KEY = os.urandom(32)
-# app.config['SECRET_KEY'] = SECRET_KEY
-# csrf = CSRFProtect()
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+csrf = CSRFProtect()
 
 @app.route('/formprueba')
 def formprueba():
@@ -17,11 +17,14 @@ def formprueba():
 @app.route('/Alumnos', methods=['GET', 'POST'])
 def Alumnos():
     reg_alum = forms.UserForm(request.form)
-    if(request.method == 'POST'):
+    datos = list()
+    if(request.method == 'POST'and reg_alum.validate()):
+        datos.append(reg_alum.matricula.data)
+        datos.append(reg_alum.nombre.data)
         print(reg_alum.matricula.data)
         print(reg_alum.nombre.data)
 
-    return render_template('Alumnos.html', form = reg_alum)
+    return render_template('Alumnos.html', form = reg_alum, datos = datos)
 
 @app.route('/', methods=['GET', 'POST'])
 def Numeros():
@@ -52,6 +55,49 @@ def valores():
 
         return render_template('valores.html', lista = lista, mayor = mayor, menor = menor, promedio = promedio, resultados = resultado)
 
+@app.route('/traductor', methods=['GET', 'POST'])
+def traductor():
+    words = forms.WordsForm(request.form)
+    palabraEncontrada = ''
+    if(request.method == 'POST' and words.validate()):
+        btnGuardar = request.form.get('btnGuardar')
+        btnTraducir = request.form.get('btnTraducir')
+        if(btnGuardar == 'Guardar'):    
+            file = open('palabras.txt', 'a')
+            file.write('\n' + words.spanish.data.upper() + '\n' + words.english.data.upper())
+            file.close()
+        if(btnTraducir == 'Traducir'):
+            opcion = request.form.get('translate')
+            file = open('palabras.txt', 'r')
+            palabras = [linea.rstrip('\n') for linea in file]
+            if(opcion == 'spanish'):
+                spanishWord = request.form.get('txtSpanish')
+                for posicion in range(len(palabras)):
+                    if(palabras[posicion] == spanishWord.upper()):
+                        palabraEncontrada = palabras[posicion - 1]
+            elif(opcion == 'english'):
+                englishWord = request.form.get('txtEnglish')
+                for posicion in range(len(palabras)):
+                    if(palabras[posicion] == englishWord.upper()):
+                        palabraEncontrada = palabras[posicion + 1]
+                        print(palabraEncontrada)
+
+    return render_template('traductor.html', form = words, palabraEncontrada = palabraEncontrada)
+
+@app.route('/cookie', methods=['GET', 'POST'])
+def cookie():
+    reg_user = forms.LoginForm(request.form)
+    response = make_response(render_template('cookie.html', form = reg_user))
+
+    if(request.method == 'POST' and reg_user.validate()):
+        user = reg_user.username.data
+        password = reg_user.password.data
+        datos = user + '@' + password
+        success_message = 'Bienvenido {}'.format(user)
+        response.set_cookie('datos_usuario', datos)
+        flash(success_message)
+    return response
+
 if __name__ == '__main__':
-    # csrf.init_app(app)
+    csrf.init_app(app)
     app.run(debug=True, port=8080)
